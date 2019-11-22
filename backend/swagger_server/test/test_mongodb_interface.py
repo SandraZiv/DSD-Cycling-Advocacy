5ccda050f27c87c0afdef3284c9fc6094af9832e
Dominik Kotarski <dojitza@gmail.com>
2019-11-22 11:55:36 +0100

    Added interface test

--- backend/swagger_server/test/test_mongodb_interface.py
+++ backend/swagger_server/test/test_mongodb_interface.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import
 
 from swagger_server.test import BaseTestCase
 from swagger_server import  mongodb_interface
-
+import random
 
 class TestMongoDbInterface(BaseTestCase):
     """
@@ -18,28 +18,15 @@ class TestMongoDbInterface(BaseTestCase):
         that it is empty
         """
 
-
-
-def randomString(stringLength=10):
-    """Generate a random string of fixed length """
-    letters = string.ascii_lowercase
-    return ''.join(random.choice(letters) for i in range(stringLength))
-
         filename = 'testfile'
-        testdata = str(random.random())
+        testdata = bytes(str(random.random()),'utf8')
         mongodb_interface.delete_file_by_filename(filename)
         mongodb_interface.insert_new_file(filename, testdata)
-        mongodb_interface.get_file_by_filename(filename)
+        gridOut = mongodb_interface.get_file_by_filename(filename)
+        self.assertEqual(retrdata,testdata)
         mongodb_interface.delete_file_by_filename(filename)
-
-        query_string = [('trip_uuid', '38400000-8cf0-11bd-b23e-10b96e4ef00d')]
-        response = self.client.open(
-            '/v1/trip/getTripByTripUUID',
-            method='GET',
-            query_string=query_string)
-        self.assert200(response,
-                       'Response body is : ' + response.data.decode('utf-8'))
-
+        gridOut = mongodb_interface.get_file_by_filename(filename)
+        self.assertEqual(gridOut, None)
 
 if __name__ == '__main__':
     import unittest
