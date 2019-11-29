20097d105a532b6f6efb0da43c7cf58b72219269
bvezmar <vezmar.boris@gmail.com>
2019-11-29 15:05:35 +0100

    Integrated RxJava and Retrofit initial work

---
+++ GpsTrackerSampleApp/app/src/main/java/com/skuzmic/gpstracker_sampleapp/retrofit/RetrofitServiceGenerator.java
@@ -0,0 +1,25 @@
+package com.skuzmic.gpstracker_sampleapp.retrofit;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitServiceGenerator {
+    private static final String BASE_URL = "";
+
+    private static Retrofit retrofit = createRetrofit();
+
+    private static Retrofit createRetrofit() {
+        Retrofit.Builder retrofitBuiler = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .client(new OkHttpClient.Builder().build());
+        return retrofitBuiler.build();
+    }
+
+    public static <S> S createService(Class<S> serviceClass) {
+        return retrofit.create(serviceClass);
+    }
+}
