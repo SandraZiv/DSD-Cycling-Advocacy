f0659928493e2bcb6b85d9a27c52b72e1272f841
Sandra Kuzmic <kuzmic.sandra1@gmail.com>
2019-12-02 15:39:22 +0100

    modify trips table

--- frontend/bumpy-web/src/components/PastTrips.js
+++ frontend/bumpy-web/src/components/PastTrips.js
@@ -1,14 +1,16 @@
 import React, {Component} from 'react';
 import {Button} from "react-bootstrap";
-import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table'
+import BootstrapTable from 'react-bootstrap-table-next'
+import paginationFactory from 'react-bootstrap-table2-paginator';
 import {UserModal} from "./UserModal";
+import './PastTrips.css'
 
 export class PastTrips extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            trips: [],
+            trips: undefined,
             shouldShowModal: false
         };
     }
@@ -24,35 +26,83 @@ export class PastTrips extends Component {
     }
 
     getTrips(deviceUUID) {
-        // fetch(`/v1/trip/getTripsByDeviceUUID?deviceUUID=${deviceUUID}`)
-        //     .then(response => response.json())
-        //     .then(json => {
-        const data = [
-            {"tripUUID": 123, "startTS": "Sandra", "endTS": "end", "distance": 12},
-            {"tripUUID": 223, "startTS": "Per", "endTS": "end", "distance": 25}
-        ];
-        // console.log(json);
-        this.setState({trips: data})
-        // }
-        // );
-    }
+        fetch(`/v1/trip/getTripsByDeviceUUID?deviceUUID=${deviceUUID}`)
+            .then(response => response.json())
+            .then(json => {
+                    const data = [
+                        {
+                            "tripUUID": "fdsf1dsf#4s-df1sdf1sd2f3f",
+                            "startTS": "2019-11-25T11:36:04Z",
+                            "endTS": "2019-11-25T12:00:51Z",
+                            "distance": 20.0,
+                            "vibration": 125
+                        },
+                        {
+                            "tripUUID": "affsf52-df1sdf1sd2f3f-asf",
+                            "startTS": "2019-11-25T11:21:17Z",
+                            "endTS": "2019-11-25T11:36:00Z",
+                            "distance": 16.7,
+                            "vibration": 191
+                        },
+                        {
+                            "tripUUID": "df1sdf1sd2f3fs56sf-sssfee",
+                            "startTS": "2019-10-25T11:36:04Z",
+                            "endTS": "2019-10-25T12:00:51Z",
+                            "distance": 42.3,
+                            "vibration": 203
+                        },
+                    ];
+                    console.log(json);
+                    this.setState({trips: data})
+                }
+            );
+    };
 
     render() {
         const {trips} = this.state;
         let tripTable = "";
-        if (trips.length !== 0) {
+        if (trips !== undefined) {
+            const columns = [{
+                dataField: 'startTS',
+                text: 'Start time',
+                sort: true
+            }, {
+                dataField: 'endTS',
+                text: 'End time',
+                sort: true
+            }, {
+                dataField: 'distance',
+                text: 'Distance',
+                sort: true
+            }, {
+                dataField: 'vibration',
+                text: 'Accumulated vibration',
+                sort: true
+            }];
+
+            const defaultSorted = [{
+                dataField: 'startTS',
+                order: 'desc'
+            }];
+
             tripTable =
-                <BootstrapTable ref='table' data={trips} pagination>
-                    <TableHeaderColumn dataField='tripUUID' isKey={true} dataSort={true}>Trip ID</TableHeaderColumn>
-                    <TableHeaderColumn dataField='startTS' dataSort={true}>Start time</TableHeaderColumn>
-                    <TableHeaderColumn dataField='endTS' dataSort={true}>End time</TableHeaderColumn>
-                    <TableHeaderColumn dataField='distance' dataSort={true}>Distance</TableHeaderColumn>
-                </BootstrapTable>
+                <BootstrapTable
+                    bootstrap4
+                    keyField="tripUUID"
+                    data={trips}
+                    columns={columns}
+                    defaultSorted={defaultSorted}
+                    noDataIndication="There is no data for given User"
+                    pagination={paginationFactory()}
+                />
         }
 
         return (
             <div>
-                <Button onClick={() => this.setState({shouldShowModal: true})}>Enter User Identifier</Button>
+                <Button className="button-margin" variant="outline-warning"
+                        onClick={() => this.setState({shouldShowModal: true})}>
+                    Enter User Identifier
+                </Button>
                 <UserModal show={this.state.shouldShowModal} onHide={this.onModalClose.bind(this)}/>
 
                 {tripTable}
