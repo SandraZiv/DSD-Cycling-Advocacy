a96928ff5eab8e4e55c7b3dde83a90943c8e1ce2
IzabellaSzydelko <izabella.szydelko@gmail.com>
2019-12-11 15:01:48 +0100

    achievements added

--- android/Bumpy/app/src/main/java/com/cycling_advocacy/bumpy/achievements/db/AchievementsInitial.java
+++ android/Bumpy/app/src/main/java/com/cycling_advocacy/bumpy/achievements/db/AchievementsInitial.java
@@ -6,7 +6,7 @@ import com.cycling_advocacy.bumpy.achievements.Achievement;
 public class AchievementsInitial {
 
     public static Achievement[] getAchievements() {
-        return new Achievement[]{a1, a2};
+        return new Achievement[]{a1, a2, a3, a4, a5, a6, a7, a8};
     }
 
     private static Achievement a1 = new Achievement(
@@ -21,5 +21,38 @@ public class AchievementsInitial {
             false
     );
 
-    // todo Izabella - add remaining achievements + update in res/values/achievements.xml
+    private static Achievement a3 = new Achievement(
+            R.string.a3_title,
+            R.string.a3_detail,
+            false
+    );
+
+    private static Achievement a4 = new Achievement(
+            R.string.a4_title,
+            R.string.a4_detail,
+            false
+    );
+    private static Achievement a5 = new Achievement(
+            R.string.a5_title,
+            R.string.a5_detail,
+            false
+    );
+
+    private static Achievement a6 = new Achievement(
+            R.string.a6_title,
+            R.string.a6_detail,
+            false
+    );
+
+    private static Achievement a7 = new Achievement(
+            R.string.a7_title,
+            R.string.a7_detail,
+            false
+    );
+
+    private static Achievement a8 = new Achievement(
+            R.string.a8_title,
+            R.string.a8_detail,
+            false
+    );
 }
