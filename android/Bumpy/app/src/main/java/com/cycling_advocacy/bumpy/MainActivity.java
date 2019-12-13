26945ae15e14cdd4c94c96c0412b57e9ed597347
Sandra Kuzmic <kuzmic.sandra1@gmail.com>
2019-12-13 13:06:28 +0100

    add toolbar

--- android/Bumpy/app/src/main/java/com/cycling_advocacy/bumpy/MainActivity.java
+++ android/Bumpy/app/src/main/java/com/cycling_advocacy/bumpy/MainActivity.java
@@ -1,12 +1,15 @@
 package com.cycling_advocacy.bumpy;
 
 import android.os.Bundle;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +17,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
--- android/Bumpy/app/src/main/java/com/cycling_advocacy/bumpy/TripInProgressActivity.java
+++ android/Bumpy/app/src/main/java/com/cycling_advocacy/bumpy/TripInProgressActivity.java
@@ -2,7 +2,9 @@ package com.cycling_advocacy.bumpy;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class TripInProgressActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class TripInProgressActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trip_in_progress);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+        }
     }
 }
