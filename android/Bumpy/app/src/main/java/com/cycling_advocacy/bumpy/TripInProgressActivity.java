babcbe00000d3249c964ec1325ed8161079ef411
salonikyal <salonikyal1996@gmail.com>
2019-12-15 11:09:31 +0100

    TripInProgress java file Added gauge view library Relevant changes in string, array and dimens :) Problem when we run the application: error in inflating the layout :(

--- android/Bumpy/app/src/main/java/com/cycling_advocacy/bumpy/TripInProgressActivity.java
+++ android/Bumpy/app/src/main/java/com/cycling_advocacy/bumpy/TripInProgressActivity.java
@@ -1,6 +1,11 @@
 package com.cycling_advocacy.bumpy;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+
+import com.ntt.customgaugeview.library.GaugeView;
+
+import java.util.Random;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +25,28 @@ public class TripInProgressActivity extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setDisplayShowHomeEnabled(true);
         }
+
+        final GaugeView gaugeViewSpeed  =  findViewById(R.id.gauge_view_speed);
+        final GaugeView gaugeViewVibration = findViewById(R.id.gauge_view_vibration);
+        gaugeViewSpeed.setShowRangeValues(true);
+        gaugeViewVibration.setShowRangeValues(true);
+        gaugeViewSpeed.setTargetValue(0);
+        gaugeViewVibration.setTargetValue(0);
+        final Random random = new Random();
+        final CountDownTimer timer = new CountDownTimer(10000, 2) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                gaugeViewSpeed.setTargetValue(random.nextInt(200));
+                gaugeViewVibration.setTargetValue(random.nextInt(100));
+            }
+
+            @Override
+            public void onFinish() {
+                gaugeViewSpeed.setTargetValue(0);
+                gaugeViewVibration.setTargetValue(0);
+            }
+        };
+        timer.start();
+
     }
 }
