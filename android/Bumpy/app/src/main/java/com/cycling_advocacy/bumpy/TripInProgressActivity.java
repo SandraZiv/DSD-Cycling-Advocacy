add7928c215a8ad0ce987d2c253ffd99f1a85ad4
Sandra Kuzmic <kuzmic.sandra1@gmail.com>
2019-12-16 10:17:44 +0100

    fix error

--- android/Bumpy/app/src/main/java/com/cycling_advocacy/bumpy/TripInProgressActivity.java
+++ android/Bumpy/app/src/main/java/com/cycling_advocacy/bumpy/TripInProgressActivity.java
@@ -3,13 +3,11 @@ package com.cycling_advocacy.bumpy;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.ntt.customgaugeview.library.GaugeView;
 
 import java.util.Random;
-
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 public class TripInProgressActivity extends AppCompatActivity {
 
@@ -17,14 +15,6 @@ public class TripInProgressActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trip_in_progress);
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-            actionBar.setDisplayShowHomeEnabled(true);
-        }
 
         final GaugeView gaugeViewSpeed  =  findViewById(R.id.gauge_view_speed);
         final GaugeView gaugeViewVibration = findViewById(R.id.gauge_view_vibration);
@@ -36,7 +26,7 @@ public class TripInProgressActivity extends AppCompatActivity {
         final CountDownTimer timer = new CountDownTimer(10000, 2) {
             @Override
             public void onTick(long millisUntilFinished) {
-                gaugeViewSpeed.setTargetValue(random.nextInt(200));
+                gaugeViewSpeed.setTargetValue(random.nextInt(50));
                 gaugeViewVibration.setTargetValue(random.nextInt(100));
             }
 
