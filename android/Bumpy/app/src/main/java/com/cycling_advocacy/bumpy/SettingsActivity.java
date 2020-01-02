a55c97b648f4ba4e0eac9079391d2af0d449db36
Sandra Kuzmic <kuzmic.sandra1@gmail.com>
2020-01-02 21:27:56 +0100

    add list preference for trip upload

--- android/Bumpy/app/src/main/java/com/cycling_advocacy/bumpy/SettingsActivity.java
+++ android/Bumpy/app/src/main/java/com/cycling_advocacy/bumpy/SettingsActivity.java
@@ -1,7 +1,10 @@
 package com.cycling_advocacy.bumpy;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
@@ -18,12 +21,19 @@ public class SettingsActivity extends AppCompatActivity {
                 .commit();
     }
 
-    public static class SettingsFragment extends PreferenceFragmentCompat {
+    public static class SettingsFragment extends PreferenceFragmentCompat
+            implements SharedPreferences.OnSharedPreferenceChangeListener {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.preferences, rootKey);
-            setUserIdentifier();
 
+            //init listener
+            getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+
+            SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
+
+            setUserIdentifier();
+            setUploadTrip(sharedPreferences);
         }
 
         private void setUserIdentifier() {
@@ -32,6 +42,36 @@ public class SettingsActivity extends AppCompatActivity {
                 String userIdentifier = PreferenceUtil.getDeviceUUID(getContext());
                 preference.setSummary(userIdentifier);
             }
+        }
+
+        // this automatically gets preference
+        // and calls overloaded method that takes list preference as argument
+        private void setUploadTrip(SharedPreferences sharedPreferences) {
+            Preference preference = findPreference(getString(R.string.pref_upload_trip_data_key));
+            if (preference != null) {
+                setUploadTripDataSummary(sharedPreferences, (ListPreference) preference);
+            }
+        }
+
+        private void setUploadTripDataSummary(SharedPreferences sharedPreferences, ListPreference listPreference) {
+            int prefIndex = listPreference.findIndexOfValue(
+                    sharedPreferences.getString(listPreference.getKey(), "0")
+            );
+            listPreference.setSummary(listPreference.getEntries()[prefIndex]);
+        }
+
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            Preference preference = findPreference(key);
+            if (preference instanceof ListPreference) {
+                setUploadTripDataSummary(sharedPreferences, (ListPreference) preference);
+            }
+        }
+
+        @Override
+        public void onDestroy() {
+            super.onDestroy();
+            getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
         }
     }
 }
