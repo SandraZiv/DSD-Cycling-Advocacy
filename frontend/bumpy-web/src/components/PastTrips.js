623e1ff3acde8ba7c5600865e4520b6c8f10efc9
Sandra Kuzmic <kuzmic.sandra1@gmail.com>
2020-01-02 15:24:35 +0100

    set past trips to save uuid from url

--- frontend/bumpy-web/src/components/PastTrips.js
+++ frontend/bumpy-web/src/components/PastTrips.js
@@ -1,89 +1,76 @@
-import React, {Component} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import BootstrapTable from 'react-bootstrap-table-next'
 import paginationFactory from 'react-bootstrap-table2-paginator';
 import {dateFormat} from "../dateformat";
+import {UuidContext} from "../Store";
 import './PastTrips.css'
 
-export class PastTrips extends Component {
+export const PastTrips = (props) => {
+    const [, setUuid] = useContext(UuidContext);
+    const [trips, setTrips] = useState(undefined);
 
-    componentDidMount() {
+    useEffect(() => {
         document.title = "Bumpy - Trips";
 
         // parse URL to get UUID
-        let uuid = this.props.location.pathname.substring('/trips/'.length);
-        this.getTrips(uuid)
+        let uuid = props.location.pathname.substring('/trips/'.length);
+        setUuid(uuid);
 
-        // todo save this
-    }
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            trips: undefined,
-        };
-    }
-
-    getTrips(deviceUUID) {
         // testing id: 5efa0f9f-ee0a-45c9-ac20-ac4bb76dc83f
-        fetch(`/v1/trip/getTripsByDeviceUUID?deviceUUID=${deviceUUID}`)
+        fetch(`/v1/trip/getTripsByDeviceUUID?deviceUUID=${uuid}`)
             .then(response => response.json())
             .then(data => {
-                let parsedTrips = data.map(function(trip) {
+                setTrips(data.map(function (trip) {
                     trip.startTS = dateFormat(new Date(trip.startTS), "dddd, mmmm dS, yyyy, HH:MM");
                     // trip.startTS = new Date(trip.startTS).toLocaleString()
                     // trip.endTS = new Date(trip.endTS).toUTCString();
                     return trip
-                });
-
-                this.setState({trips: parsedTrips})
+                }));
             });
-    };
+    });
 
-    render() {
-        const {trips} = this.state;
-        let tripTable = "";
-        if (trips !== undefined) {
-            // console.log(trips)
+    let tripTable = "";
+    if (trips !== undefined) {
+        // console.log(trips)
 
-            const columns = [{
-                dataField: 'startTS',
-                text: 'Start time',
-                sort: true
-            }, {
-                dataField: 'endTS',
-                text: 'End time',
-                sort: true
-            }, {
-                dataField: 'distance',
-                text: 'Distance(km)',
-                sort: true
+        const columns = [{
+            dataField: 'startTS',
+            text: 'Start time',
+            sort: true
+        }, {
+            dataField: 'endTS',
+            text: 'End time',
+            sort: true
+        }, {
+            dataField: 'distance',
+            text: 'Distance(km)',
+            sort: true
             // }, {
             //     dataField: 'vibration',
             //     text: 'Average vibration(%)',
             //     sort: true
-            }];
+        }];
 
-            const defaultSorted = [{
-                dataField: 'startTS',
-                order: 'desc'
-            }];
+        const defaultSorted = [{
+            dataField: 'startTS',
+            order: 'desc'
+        }];
 
-            tripTable =
-                <BootstrapTable
-                    bootstrap4
-                    keyField="tripUUID"
-                    data={trips}
-                    columns={columns}
-                    defaultSorted={defaultSorted}
-                    noDataIndication="There is no data for given User"
-                    pagination={paginationFactory()}
-                />
-        }
-
-        return (
-            <div>
-                {tripTable}
-            </div>
-        )
+        tripTable =
+            <BootstrapTable
+                bootstrap4
+                keyField="tripUUID"
+                data={trips}
+                columns={columns}
+                defaultSorted={defaultSorted}
+                noDataIndication="There is no data for given User"
+                pagination={paginationFactory()}
+            />
     }
-}
+
+    return (
+        <div>
+            {tripTable}
+        </div>
+    )
+};
