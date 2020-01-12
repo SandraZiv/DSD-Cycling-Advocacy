fd5274e2d7ba7f3e3e85d615b2467355d16d30a8
bvezmar <vezmar.boris@gmail.com>
2020-01-12 20:10:57 +0100

    Updated PastTripDetailedResponse model

--- android/Bumpy/app/src/main/java/com/cycling_advocacy/bumpy/net/model/PastTripDetailedResponse.java
+++ android/Bumpy/app/src/main/java/com/cycling_advocacy/bumpy/net/model/PastTripDetailedResponse.java
@@ -27,17 +27,25 @@ public class PastTripDetailedResponse {
     @SerializedName("elevation")
     private Elevation elevation;
 
+    @SerializedName("vibration")
+    private Vibration vibration;
+
     @SerializedName("gnssData")
     private List<GnssData> gnssData;
 
-    public PastTripDetailedResponse(String tripUUID, Date startTS, Date endTS, double distance, Speed speed, Elevation elevation, List<GnssData> gnssData) {
+    @SerializedName("bumpyPoints")
+    private List<BumpyPoint> bumpyPoints;
+
+    public PastTripDetailedResponse(String tripUUID, Date startTS, Date endTS, double distance, Speed speed, Elevation elevation, Vibration vibration, List<GnssData> gnssData, List<BumpyPoint> bumpyPoints) {
         this.tripUUID = tripUUID;
         this.startTS = startTS;
         this.endTS = endTS;
         this.distance = distance;
         this.speed = speed;
         this.elevation = elevation;
+        this.vibration = vibration;
         this.gnssData = gnssData;
+        this.bumpyPoints = bumpyPoints;
     }
 
     public String getTripUUID() { return tripUUID; }
@@ -64,9 +72,17 @@ public class PastTripDetailedResponse {
 
     public void setElevation(Elevation elevation) { this.elevation = elevation; }
 
+    public Vibration getVibration() { return vibration; }
+
+    public void setVibration(Vibration vibration) { this.vibration = vibration; }
+
     public List<GnssData> getGnssData() { return gnssData; }
 
     public void setGnssData(List<GnssData> gnssData) { this.gnssData = gnssData; }
+
+    public List<BumpyPoint> getBumpyPoints() { return bumpyPoints; }
+
+    public void setBumpyPoints(List<BumpyPoint> bumpyPoints) { this.bumpyPoints = bumpyPoints; }
 
     public class Speed {
 
@@ -118,5 +134,65 @@ public class PastTripDetailedResponse {
         public double getAvgElevation() { return avgElevation; }
 
         public void setAvgElevation(double avgElevation) { this.avgElevation = avgElevation; }
+    }
+
+    public class Vibration {
+
+        @SerializedName("minVibration")
+        private double minVibration;
+
+        @SerializedName("maxVibration")
+        private double maxVibration;
+
+        @SerializedName("avgVibration")
+        private double avgVibration;
+
+        public Vibration(double minVibration, double maxVibration, double avgVibration) {
+            this.minVibration = minVibration;
+            this.maxVibration = maxVibration;
+            this.avgVibration = avgVibration;
+        }
+
+        public double getMinVibration() { return minVibration; }
+
+        public void setMinVibration(double minVibration) { this.minVibration = minVibration; }
+
+        public double getMaxVibration() { return maxVibration; }
+
+        public void setMaxVibration(double maxVibration) { this.maxVibration = maxVibration; }
+
+        public double getAvgVibration() { return avgVibration; }
+
+        public void setAvgVibration(double avgVibration) { this.avgVibration = avgVibration; }
+    }
+
+    public class BumpyPoint {
+
+        @SerializedName("bumpyScore")
+        private double bumpyScore;
+
+        @SerializedName("lat")
+        private double lat;
+
+        @SerializedName("lon")
+        private double lon;
+
+        public BumpyPoint(double bumpyScore, double lat, double lon) {
+            this.bumpyScore = bumpyScore;
+            this.lat = lat;
+            this.lon = lon;
+        }
+
+        public double getBumpyScore() { return bumpyScore; }
+
+        public void setBumpyScore(double bumpyScore) { this.bumpyScore = bumpyScore; }
+
+        public double getLat() { return lat; }
+
+        public void setLat(double lat) { this.lat = lat; }
+
+        public double getLon() { return lon; }
+
+        public void setLon(double lon) { this.lon = lon; }
     }
 }
