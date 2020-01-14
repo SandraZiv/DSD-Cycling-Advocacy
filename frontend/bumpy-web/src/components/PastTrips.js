17e064e4302bfe5d881058a0087e8ac70707cbe5
Sandra Kuzmic <kuzmic.sandra1@gmail.com>
2020-01-14 11:42:32 +0100

    little more formatting :)

--- frontend/bumpy-web/src/components/PastTrips.js
+++ frontend/bumpy-web/src/components/PastTrips.js
@@ -47,7 +47,7 @@ export const PastTrips = (props) => {
                             trip.startTS = formatDateDefault(trip.startTS);
                             trip.endTS = formatDateDefault(trip.endTS);
                             trip.distance = formatFloat(trip.distance);
-                            trip.avgVibration = trip.vibration.avgVibration;
+                            trip.avgVibration = formatFloat(trip.vibration.avgVibration);
                             return trip
                         }));
                     });
