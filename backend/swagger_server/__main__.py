f67d0d1b7613410983fce72261361adbc00b98d4
Dominik Kotarski <dojitza@gmail.com>
2020-01-17 17:16:35 +0100

    wsgi compliance

--- backend/swagger_server/__main__.py
+++ backend/swagger_server/__main__.py
@@ -1,28 +1,9 @@
 #!/usr/bin/env python3
 
-import connexion
+from swagger_server import app
 
-from swagger_server import encoder, constants as const
-from swagger_server.road_analysis import queue
-import logging
-
-
-def main():
-
-    # logger setup (application-wide)
-    logging.basicConfig(filename='server_log.log', level=logging.INFO,
-                        format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-
-    # connect to a running rabbitmq server and executes incoming jobs
-    queue_listener = queue.Listener(const.TRIP_ANALYSIS_QUEUE, const.RABBITMQ_HOST)
-    queue_listener.start_consuming()
-
-    # flask based api endpoint, powered by swagger server
-    app = connexion.App(__name__, specification_dir='./swagger/')
-    app.app.json_encoder = encoder.JSONEncoder
-    app.add_api('swagger.yaml', arguments={'title': 'Bumpy API'}, pythonic_params=True)
+def run():
     app.run(port=5000, debug=True)
 
-
 if __name__ == '__main__':
-    main()
+    run()
