f3ac7c46b53169f06ca76de40b0660ca329a2005
Dominik Kotarski <dojitza@gmail.com>
2020-01-24 18:44:40 +0100

    Small fix

--- backend/swagger_server/mongodb_interface.py
+++ backend/swagger_server/mongodb_interface.py
@@ -3,7 +3,7 @@ from bson.objectid import ObjectId
 import gridfs
 import pymongo
 
-mongodbUri = 'mongodb+srv://app:kk1AVXNUwsAoqnyx@bumpy-tdkiu.gcp.mongodb.net/test?retryWrites=true&w=majority''
+mongodbUri = 'mongodb+srv://app:kk1AVXNUwsAoqnyx@bumpy-tdkiu.gcp.mongodb.net/test?retryWrites=true&w=majority'
 
 #app = Flask(__name__)
 client = pymongo.MongoClient(mongodbUri, maxPoolSize=10, connect=False)
