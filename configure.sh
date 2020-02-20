d1335073f0373ab24a0f61b673d3edc88a61f1e5
Dominik Kotarski <dojitza@gmail.com>
2020-02-20 20:56:30 +0100

    First complete version of configure.sh

--- configure.sh
+++ configure.sh
@@ -64,4 +64,45 @@ window['runConfig'] = {
 }
 EOF
 
-#configure android 
+#configure android
+cat << EOF > $DIR/android/Bumpy/app/src/main/java/com/cycling_advocacy/bumpy/net/service/BumpyServiceBuilder.java
+
+package com.cycling_advocacy.bumpy.net.service;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.Date;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class BumpyServiceBuilder {
+
+    private static final String BASE_URL = "http://$ipname:$port/$prefix/api/v1/";
+
+    private static Retrofit retrofit = createRetrofit();
+
+    private static Retrofit createRetrofit() {
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(Date.class, new GsonUTC())
+                .create();
+
+        Retrofit.Builder retrofitBuiler = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .client(new OkHttpClient.Builder().build());
+        return retrofitBuiler.build();
+    }
+
+    public static <S> S createService(Class<S> serviceClass) {
+        return retrofit.create(serviceClass);
+    }
+}
+EOF
+
+echo "Configuration complete. Your app has been configured to be served from $ipname:$port/$prefix"
+echo "Android app has been configured as well. You may build and publish it"
